f42222b7efec7d7ec8603c647a456c2d47924aa8
veerbal1 <veerbalsingh1@gmail.com>
2021-09-07 21:34:42 +0530

    Created basic todo list item

--- src/Components/ActiveList.js
+++ src/Components/ActiveList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ListWrapper from "./ListWrapper";
+import TodoListItem from "./TodoListItem";
 
 function ActiveList() {
-  return <ListWrapper title="Active">
-    <p>to do</p>
-  </ListWrapper>;
+  return (
+    <ListWrapper title="Active">
+      <TodoListItem value="Learning React"/>
+      <TodoListItem value="Learning Next"/>
+    </ListWrapper>
+  );
 }
 
 export default ActiveList;
--- src/Components/TodoListItem.js
+++ src/Components/TodoListItem.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function TodoListItem() {
-    return (
-        <div>
-            <TodoListItem/>
-        </div>
-    )
+const styles = {
+  glassEffect: {
+    background: "rgba(255,255,255,0.1)",
+    backdropFilter: "saturate(180%) blur(10px)",
+  },
+};
+
+function TodoListItem({ value }) {
+  return (
+    <div
+      className="text-sm 
+                font-extralight 
+                flex 
+                justify-start 
+                items-center 
+                px-2 
+                py-1 
+                select-none 
+                cursor-pointer
+                my-1
+                "
+      style={styles.glassEffect}
+    >
+      {value}
+    </div>
+  );
 }
 
-export default TodoListItem
+export default TodoListItem;
