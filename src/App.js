9552f3530591819add48a72d8e559757f7e82e26
veerbal1 <veerbalsingh1@gmail.com>
2021-09-07 19:17:03 +0530

    Added main background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./App.css";
+import backgroundImage from "./assets/bg.jpg";
+
+const styles = {
+  main: {
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundColor: "#B9A9A1",
+  },
+};
 
 function App() {
   return (
-    <div>
+    <div style={styles.main}>
       <h1>React Todo List</h1>
     </div>
   );
